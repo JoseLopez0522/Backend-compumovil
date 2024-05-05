bb80c9c6fa74ad8d6c61e0c7afb443543889572f
blanco <dehara14@gmail.com>
2024-05-05 11:12:42 -0600

    validaciones

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -7,27 +7,17 @@ export const verifyAdmin = async (req, res, next) => {
   try {
     const { authorization } = req.headers;
     const token = authorization ? authorization.replace('Bearer ', '') : null;
-
+    const userAuth = await Jwt.verify(token, process.env.PRIVATE_KEY, {
+      expiresIn: 60 * 60 * 24
+    });
     if (!token) {
       return res.status(403).json({ ok: false, msg: 'No token' });
-    } else {
-      next();
     }
-
-    /* const userAuth = await Jwt.verify(token, process.env.PRIVATE_KEY, {
-      expiresIn: 60 * 60 * 24
-    }); */
-
-    /* if (userAuth) {
-      const permisos = await RolesAndPermisos.findAll({
-        where: { roleRol: userAuth.rol }
-      });
-      req.usuario = userAuth;
-      req.permisos = permisos.map((el) => el.dataValues.permisoIdpermiso);
+    if (userAuth) {
       next();
     } else {
       res.status(403).json({ ok: false, msg: 'No autorizado' });
-    } */
+    }
   } catch (err) {
     res.status(403).json({ ok: false, msg: 'No autorizado' });
   }
--- src/routes/index.route.ts
+++ src/routes/index.route.ts
@@ -4,12 +4,13 @@ import carritos from '@routes/Carritos.route';
 import Productos from '@routes/Productos.route';
 import Pagos from '@routes/Pagos.route';
 import formadepagos from '@routes/Formadepagos.route';
+import { verifyAdmin } from '@middlewares/auth';
 
 const router = Router();
 
 router.use('/usuarios', Usuarios);
-router.use('/carritos', carritos);
-router.use('/productos', Productos);
+router.use('/carritos', verifyAdmin, carritos);
+router.use('/productos', verifyAdmin, Productos);
 router.use('/pagos', Pagos);
 router.use('/Formadepagos', formadepagos);
 export default router;
