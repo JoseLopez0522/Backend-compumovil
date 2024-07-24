724f197da78368cee97d840533aa044e60be1032
JesusEhuan <jm_hdz@outlook.com>
2024-07-24 11:43:13 -0600

    Ajustes
    Ajuste

--- src/config/db.config.ts
+++ src/config/db.config.ts
@@ -1,20 +1,27 @@
 import { Sequelize } from 'sequelize';
 import { config } from 'dotenv';
-config();
+
+config(); // Cargar variables de entorno
+
 const sequelize = new Sequelize(
-  process.env.NAME_DB,
-  process.env.USER_DB,
-  process.env.PASSWORD_DB,
+  process.env.NAME_DB,       // Nombre de la base de datos
+  process.env.USER_DB,       // Usuario de la base de datos
+  process.env.PASSWORD_DB,   // Contraseña de la base de datos
   {
-    host: process.env.HOST_DB,
-    port: Number(process.env.PORT_DB),
-    dialect: 'mysql'
+    host: process.env.HOST_DB,   // Host de la base de datos
+    dialect: 'mssql',           // Dialecto especificado para SQL Server
+    dialectOptions: {
+      options: {
+        encrypt: false, // Desactivar el cifrado si no es necesario
+        trustServerCertificate: false, // Si no se utiliza un certificado de servidor confiable
+      },
+    },
   }
 );
 
 sequelize
   .authenticate()
-  .then(() => console.log('conectado correctamente a la base de datos'))
-  .catch((err) => console.log(err));
+  .then(() => console.log('Conectado correctamente a la base de datos'))
+  .catch((err) => console.log('Error al conectar a la base de datos:', err));
 
 export default sequelize;
